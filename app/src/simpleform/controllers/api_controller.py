b1c397cfbf7bf4fe222805e735c16e208ab64fa3
Luis Guilherme <luisgsouza01@gmail.com>
2018-05-19 13:18:00 -0300

    Script startup cluster swarm

--- app/src/simpleform/controllers/api_controller.py
+++ app/src/simpleform/controllers/api_controller.py
@@ -3,6 +3,7 @@
 from flask import jsonify
 
 from simpleform import app
+import socket
 
 @app.route("/api/v1/info", methods = ['GET'])
 def home_index():
@@ -11,7 +12,8 @@ def home_index():
             'version'   : 'v1',
             'buildtime' : '2018-05-19',
             'links'     : '/api/v1/sugestoes',
-            'methods'   : 'get, post, put, delete'
+            'methods'   : 'get, post, put, delete',
+            'host'      : socket.gethostname()
         }
     ]
     return jsonify({'api_version': api_list}), 200
